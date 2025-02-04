d12d8b97ccf2a8d64e7401b15cb1a6152c264565
muskan rajoriya <144930686+mussu-2360@users.noreply.github.com>
2025-02-04 13:22:16 +0530

    Add files via upload

---
+++ UploadFiles.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UploadFiles.aspx.cs" Inherits="FileUploading.UploadFiles" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Upload Multiple Files</title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="fileUpload" runat="server" AllowMultiple="true" />
+            <asp:Button ID="btnUpload" runat="server" Text="Upload"  OnClick="btnUpload_Click" />
+            <asp:Label ID="lblStatus" runat="server" Text="" ForeColor="Green"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UploadFiles.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUploading
+{
+    public partial class UploadFiles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (fileUpload.HasFiles)
+            {
+                // Generate a new folder name based on the current timestamp
+                string folderName = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string folderPath = Server.MapPath("~/Uploads/" + folderName);
+
+                try
+                {
+                    // Create the folder if it doesn't exist
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+
+                    // Save each file to the newly created folder
+                    foreach (var file in fileUpload.PostedFiles)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string filePath = Path.Combine(folderPath, fileName);
+                        file.SaveAs(filePath);
+                    }
+
+                    lblStatus.Text = $"Files uploaded successfully to folder: {folderName}";
+                    lblStatus.ForeColor = System.Drawing.Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "Error: " + ex.Message;
+                    lblStatus.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            else
+            {
+                   lblStatus.Text = "No files selected.";
+                   lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+            
+
+
+
+        }
+
+    }
+}
